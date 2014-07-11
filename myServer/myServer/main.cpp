36467deaf5f7b29bb3a532a003a3e97d697df8f6
signorinotang <signorinotang@sina.cn>
2014-07-12 07:39:02 +0800

    Initial Commit

---
+++ myServer/myServer/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  myServer
+//
+//  Created by signorinotang on 14-7-12.
+//  Copyright (c) 2014年 signorinotang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
