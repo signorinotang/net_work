fd7daa1c8b7755638ae9acdc8cbab557ba9082a1
signorinotang <signorinotang@sina.cn>
2014-07-12 20:17:07 +0800

    update myClient and my Server

---
+++ myServer/myServer/Tpye.h
@@ -0,0 +1,16 @@
+//
+//  Tpye.h
+//  myServer
+//
+//  Created by signorinotang on 14-7-12.
+//  Copyright (c) 2014 signorinotang. All rights reserved.
+//
+
+#ifndef myServer_Tpye_h
+#define myServer_Tpye_h
+
+
+
+
+
+#endif
